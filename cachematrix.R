23fcd4585a4381a1e1eaddd51f5346ecc04d515e
Peter Erös <erospeti@gmail.com>
2014-06-11 11:32:24 +0200

    Coursera RProg Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Use following functions to calculate and cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix: initializes the list of handler functions:
+## set(matrix), get(), setinv(inverse), getinv()
+## Use: initialize with v <- makeCacheMatrix(), load matrix with v$set(X)
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function() {
+      m <- NULL
+      x <- matrix()
+      
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      
+      get <- function() x
+      
+      setinv <- function(inv) m <<- inv
+      
+      getinv <- function() m
+      
+      ## Return: list of functions
+      list(set = set,
+           get = get,
+           setinv = setinv,
+           getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: returns the inverse of a matrix
+## Only calculated if it is not cached already
+## Use: cacheSolve(v) ## v initialized with makeCacheMatrix()
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+      
+      ## Load inverse from cache
+      m <- x$getinv()
+      
+      ## Retrieve if already cached
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      
+      ## Load matrix if not cached already
+      data <- x$get()
+      
+      ## Calculate inverse
+      m <- solve(data)
+      
+      ## Store in cache
+      x$setinv(m)
+      
+      m
 }
